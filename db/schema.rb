a8fc6f3e65e0d5d1f1b388618213e2b250759e4f
willowcheng <e.wingus@gmail.com>
2017-10-31 17:17:04 -0400

    Create a model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727201448) do
+ActiveRecord::Schema.define(version: 20171031211007) do
+
+  create_table "pages", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.string   "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
